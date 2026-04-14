124162196767809b1f8a1f53ec7f156706fb4bfb
Paridhi Gupta <paridhigupta@Paridhis-MacBook-Air.local>
2026-04-14 17:55:37 +0530

    day-2 learning

--- app.js
+++ app.js
@@ -6,20 +6,28 @@ import ReactDOM from "react-dom/client";
 //     id:"heading"
 // }, "Hello World...!!!")
 
-const parent = React.createElement("div",
-    { id: "parent" },
-    [
-        React.createElement("div", { id: "child1", key: "child-1" },
-            [
-                React.createElement("h1", { key: "c1-h1" }, "I am h1 tag"),
-                React.createElement("h2", { key: "c1-h2" }, "I am h2 tag")
-            ]),
-        React.createElement("div", { id: "child2", key: "child-2" },
-            [
-                React.createElement("h1", { key: "c2-h1" }, "I am h1 tag"),
-                React.createElement("h2", { key: "c2-h2" }, "I am h2 tag")
-            ])
-    ]
-)
+// const parent = React.createElement("div",
+//     { id: "parent" },
+//     [
+//         React.createElement("div", { id: "child1", key: "child-1" },
+//             [
+//                 React.createElement("h1", { key: "c1-h1" }, "I am h1 tag"),
+//                 React.createElement("h2", { key: "c1-h2" }, "I am h2 tag")
+//             ]),
+//         React.createElement("div", { id: "child2", key: "child-2" },
+//             [
+//                 React.createElement("h1", { key: "c2-h1" }, "I am h1 tag"),
+//                 React.createElement("h2", { key: "c2-h2" }, "I am h2 tag")
+//             ])
+//     ]
+// )
+
+const jsxHeading = <h1 id="heading">Hello World...!!!</h1>
+
+const HeadingComponent = () => {
+    return (
+        <h1 id="heading">Hello World........!!!</h1>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
